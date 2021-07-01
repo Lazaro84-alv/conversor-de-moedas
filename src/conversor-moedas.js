c4cd8de2a024d5c6c13789298195320b1c7d8240
Lázaro Alves <geo.eolo@gmail.com>
2021-07-01 17:56:25 -0300

    Adiciona o componente principal

---
+++ src/conversor-moedas.js
@@ -0,0 +1,9 @@
+import './conversor-moedas.css';
+
+function ConversorMoedas() {
+  return (
+   <h1>Conversor de moedas</h1>
+  );
+}
+
+export default ConversorMoedas;
--- src/conversor-moedas.test.js
+++ src/conversor-moedas.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import ConversorMoedas from './conversor-moedas';
 
-test('renders learn react link', () => {
-  render(<App />);
+test('deve renderizar o componente sem erros', () => {
+  render(<ConversorMoedas />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
