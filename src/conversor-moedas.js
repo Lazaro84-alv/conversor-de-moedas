39d86d59cd4542d473ef205dbb8d7cfe929717c0
Lázaro Alves <geo.eolo@gmail.com>
2021-07-02 02:04:59 -0300

    Implementa os testes unitários

--- src/conversor-moedas.js
+++ src/conversor-moedas.js
@@ -109,7 +109,7 @@ function ConversorMoedas() {
               </Form.Control>
             </Col>
             <Col sm="2">
-              <Button variant="success" type="submit">
+              <Button variant="success" type="submit" data-testid="btn-converter">
                 <span className={exibirSpinner ? null : 'hidden'}>
                     <Spinner animation="border" size="sm" />
                 </span>
@@ -120,7 +120,7 @@ function ConversorMoedas() {
             </Col>
           </Form.Row>
         </Form>
-        <Modal show={exibirModal} onHide={handleFecharModal}>
+        <Modal show={exibirModal} onHide={handleFecharModal} data-testid="modal">
           <Modal.Header closeButton>
             <Modal.Title>Conversão</Modal.Title>
           </Modal.Header>
