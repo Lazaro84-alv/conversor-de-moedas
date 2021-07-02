ce8ed8e68d7d6c79819cc2fe41c6a7303b576166
Lázaro Alves <geo.eolo@gmail.com>
2021-07-02 01:10:54 -0300

    Adiciona a requisição de conversão com o Axios

--- src/conversor-moedas.js
+++ src/conversor-moedas.js
@@ -3,9 +3,12 @@ import './conversor-moedas.css';
 import { Jumbotron, Button, Form, Col, Spinner, Alert, Modal } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
-import ListarMoedas from './listar-moedas'; 
+import ListarMoedas from './listar-moedas';
+import axios from 'axios'; 
 
 function ConversorMoedas() {
+
+  const FIXER_URL = 'http://data.fixer.io/api/latest?access_key=eba7130a5b2d720ce43eb5fcddd47cc3';
 
   const [valor, setValor] = useState('1');
   const [moedaDe, setMoedaDe] = useState('BRL');
@@ -39,9 +42,25 @@ function ConversorMoedas() {
     event.preventDefault();
     setFormValidado(true);
     if (event.currentTarget.checkValidity() === true) {
-      //TODO implementar a chamada ao Fixer.io
-      setExibirModal(true);
+      setExibirSpinner(true);
+      axios.get(FIXER_URL)
+        .then(res => {
+          const cotacao = obterCotacao(res.data);
+          setResultadoConversao(`${valor} ${moedaDe} = ${cotacao} ${moedaPara}`);
+          setExibirModal(true);
+          setExibirSpinner(false);
+        })
     } 
+  }
+
+  function obterCotacao(dadosCotacao) {
+    if (!dadosCotacao || dadosCotacao.success !== true) {
+      return false;
+    }
+    const cotacaoDe = dadosCotacao.rates[moedaDe];
+    const cotacaoPara = dadosCotacao.rates[moedaPara];
+    const cotacao = (1 / cotacaoDe * cotacaoPara) * valor;
+    return cotacao.toFixed(2);
   }
 
   return (
